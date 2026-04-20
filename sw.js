8bc993aeec10f677ba7717431aeaefe91f58d815
Neverwhere01 <hugo_villanueva@live.com>
2026-04-20 21:09:27 +0200

    Create iPhone-friendly basketball routine app with exercise checklists and YouTube links

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "gymchecks-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE = "jumpready-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
@@ -7,11 +7,13 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
